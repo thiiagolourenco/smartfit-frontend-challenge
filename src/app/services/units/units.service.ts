7153bc19141bc07e9c2af9bdb2137dfb17412aa8
t.bezerra <t.bezerra@samsung.com>
2024-02-14 17:56:37 -0300

    Create get all units methods

--- src/app/services/units/units.service.ts
+++ src/app/services/units/units.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UnitsResponse } from 'src/app/models/units-response.model';
+import { environment } from 'src/app/environments/environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnitsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllUnits(): Observable<UnitsResponse> {
+    return this.http.get<UnitsResponse>(
+      `${environment.URL_API}/locations.json`
+    );
+  }
 }
