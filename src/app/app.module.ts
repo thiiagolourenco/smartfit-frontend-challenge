09818899ee30399eb6a20f1f9aa3e8b3bf890e76
t.bezerra <t.bezerra@samsung.com>
2024-02-15 16:53:01 -0300

    Organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SubtitleComponent } from './components/subtitle/subtitle.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
 import { UnitsService } from './services/units/units.service';
 import { FilterService } from './services/filter/filter.service';
-import { FooterComponent } from './components/footer/footer.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { SubtitleComponent } from './components/subtitle/subtitle.component';
-
 @NgModule({
   declarations: [
     AppComponent,
