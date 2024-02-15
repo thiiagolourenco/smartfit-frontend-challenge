a96643cbc1bc61d624cfefd6bcdce5619a16354a
t.bezerra <t.bezerra@samsung.com>
2024-02-15 15:52:18 -0300

    Create input variabel to units

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Units } from 'src/app/models/units.model';
 
 @Component({
   selector: 'smartfit-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent {
+  @Input() units: Units[];
 
+  constructor() {
+    this.units = [];
+  }
 }
